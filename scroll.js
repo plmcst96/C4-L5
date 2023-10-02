aa6041ca6174d36937014d3679c9c7e4f88ace70
Cristina Palmisani <cristina.palmisani@outlook.it>
2023-10-02 12:36:50 +0200

    md

--- scroll.js
+++ scroll.js
@@ -10,3 +10,15 @@ window.addEventListener('scroll', function () {
         button.style.backgroundColor = "#191919";
     }
 });
+
+const letterM = function () {
+    const element0 = document.querySelectorAll("svg > g > g > g[opacity = '0']");
+    const element1 = document.querySelectorAll("svg > g > g > g[opacity = '1']");
+    let randomEl = Math.floor(Math.random() * element0.length);
+    element0[randomEl].setAttribute("opacity", "1");
+
+    randomEl = Math.floor(Math.random() * element1.length);
+    element1[randomEl].setAttribute("opacity", "0");
+
+}
+setInterval(letterM, 200);
